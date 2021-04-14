4bce819c1fd5d425bb7476b5723cc73396e907ad
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-04-14 18:05:31 +0530

    Create Realtime_Database.java

---
+++ firebase/Authentication/app/src/main/java/com/pratik/authentication/Realtime_Database.java
@@ -0,0 +1,73 @@
+package com.pratik.authentication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class Realtime_Database extends AppCompatActivity {
+
+    private EditText title,description,author;
+    private Button save;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realtime__database);
+
+        getSupportActionBar().hide();
+
+        title = findViewById(R.id.EditText_Title);
+        description = findViewById(R.id.EditText_description);
+        author = findViewById(R.id.EditText_Author);
+        save = findViewById(R.id.save);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                HashMap<String,Object> map = new HashMap<>();
+                map.put("title",title.getText().toString());
+                map.put("description",description.getText().toString());
+                map.put("author",author.getText().toString());
+
+                FirebaseDatabase.getInstance().getReference().child("Post").push()
+                        .setValue(map)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Log.i("jfbvkj", "onComplete: ");
+                                Toast.makeText(Realtime_Database.this, "Data Save..", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.i("jfbvkj", "onFailure: "+e.toString());
+                                Toast.makeText(Realtime_Database.this, e.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.i("jfbvkj", "onSuccess: ");
+                    }
+                });
+
+
+            }
+        });
+    }
+}
