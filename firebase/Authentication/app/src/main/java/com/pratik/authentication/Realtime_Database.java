9a2b8b019e59d5e2619fc83fdf202ce55ecaa10b
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-04-14 18:19:22 +0530

    Update Realtime_Database.java

--- firebase/Authentication/app/src/main/java/com/pratik/authentication/Realtime_Database.java
+++ firebase/Authentication/app/src/main/java/com/pratik/authentication/Realtime_Database.java
@@ -19,31 +19,24 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 
 public class Realtime_Database extends AppCompatActivity {
-
     private EditText title,description,author;
     private Button save;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_realtime__database);
-
         getSupportActionBar().hide();
-
         title = findViewById(R.id.EditText_Title);
         description = findViewById(R.id.EditText_description);
         author = findViewById(R.id.EditText_Author);
         save = findViewById(R.id.save);
-
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 HashMap<String,Object> map = new HashMap<>();
                 map.put("title",title.getText().toString());
                 map.put("description",description.getText().toString());
                 map.put("author",author.getText().toString());
-
                 FirebaseDatabase.getInstance().getReference().child("Post").push()
                         .setValue(map)
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -65,8 +58,6 @@ public class Realtime_Database extends AppCompatActivity {
                         Log.i("jfbvkj", "onSuccess: ");
                     }
                 });
-
-
             }
         });
     }
