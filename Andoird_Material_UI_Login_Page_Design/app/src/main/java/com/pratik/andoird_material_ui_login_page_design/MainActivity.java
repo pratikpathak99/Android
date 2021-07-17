dbd5b6befb27a82f5d501975ae39391f7feb2d90
Pratik Pathak <46027754+pratikpathak99@users.noreply.github.com>
2021-07-17 20:45:37 +0530

    Create MainActivity.java

---
+++ Andoird_Material_UI_Login_Page_Design/app/src/main/java/com/pratik/andoird_material_ui_login_page_design/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pratik.andoird_material_ui_login_page_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+    }
+}
